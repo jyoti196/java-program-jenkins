2db45fe0d69d271d27c1ccb208ecc55f42b289f7
jyoti <jyoti6919@gmail.com>
2026-04-16 04:43:51 +0530

    to practice jenkins

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         System.out.println(new Date());
+        System.out.println("Good Morning!!");
         }
     }
